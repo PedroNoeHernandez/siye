8b5a6682b46458e9f486f460512a03a7fc82578e
peterscrum <pedronoe@comunidad.unam.mx>
2019-09-16 11:13:07 -0400

    Cambios y correcciones al header y footer, home listo para revision

--- siye/src/app/home-card/home-card.component.ts
+++ siye/src/app/home-card/home-card.component.ts
@@ -9,12 +9,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeCardComponent implements OnInit {
   apps: Array<any>= [
-    {nameApp:'Intranet',urlApp:'',imageApp:'../../assets/img/LogosHome/logoUnamCanada.png'},
-    {nameApp:'OneDrive',urlApp:'',imageApp:'../../assets/img/LogosHome/OneDriveLogo.png'},
+    {nameApp:'Intranet',urlApp:'https://canada.unam.mx/intranet/',imageApp:'../../assets/img/LogosHome/logoUnamCanada.png'},
+    {nameApp:'OneDrive',urlApp:'https://unamcanada-my.sharepoint.com/_layouts/15/MySite.aspx?MySiteRedirect=AllDocuments',imageApp:'../../assets/img/LogosHome/OneDriveLogo.png'},
     {nameApp:'Dashboard',urlApp:'',imageApp:'../../assets/img/LogosHome/graph.png'},
-    {nameApp:'Forms',urlApp:'',imageApp:'../../assets/img/LogosHome/forms.png'},
-    {nameApp:'Sitio Web',urlApp:'',imageApp:'../../assets/img/LogosHome/logoUnamCanada.png'},
-    {nameApp:'Calendario',urlApp:'',imageApp:'../../assets/img/LogosHome/logoUnamCanada.png'}
+    {nameApp:'Forms',urlApp:'https://forms.office.com/',imageApp:'../../assets/img/LogosHome/forms.png'},
+    {nameApp:'Sitio Web',urlApp:'https://canada.unam.mx/es/idiomas/',imageApp:'../../assets/img/LogosHome/logoUnamCanada.png'},
+    {nameApp:'Calendario',urlApp:'https://canada.unam.mx/intranet/calendario/',imageApp:'../../assets/img/LogosHome/logoUnamCanada.png'},
+    {nameApp:'Outlook',urlApp:'https://outlook.office365.com/mail/inbox',imageApp:'../../assets/img/LogosHome/Outlooklogo.png'}
   ];
   constructor() { }
 
